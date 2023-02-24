abeb5606dbacb9b8a62e91248a6152c5ab7355c7
Hanselkek <87748222+Hanselkek@users.noreply.github.com>
2023-02-24 17:50:26 +0800

    Added return types

--- ListAnalyzer.py
+++ ListAnalyzer.py
@@ -1,7 +1,7 @@
 list_count = 0
 
 class ListAnalyzer:
-    def __init__(self, ListName: str):
+    def __init__(self, ListName: str) -> None:
         global list_count
 
         list_count += 1
@@ -9,9 +9,9 @@ class ListAnalyzer:
         self.listName = ListName or f"RandList{list_count}"
         self.list = []
 
-    def insert(self, a):
+    def insert(self, a) -> None:
         assert a != None, TypeError
         self.list.append(a)
 
-    def get_count(self):
+    def get_count(self) -> int:
         return len(self.list)
