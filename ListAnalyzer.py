e8c3119049c59c5ccdcba078473ceebac69e6bc6
Hanselkek <87748222+Hanselkek@users.noreply.github.com>
2023-02-24 16:40:15 +0800

    Finished tests.py, changes to Api

--- ListAnalyzer.py
+++ ListAnalyzer.py
@@ -9,10 +9,12 @@ class ListAnalyzer:
         self.listName = ListName or f"RandList{list_count}"
         self.list = []
 
-    def remove(self):
+    def delete(self):
         list_count -= 1
-
+        # Rewrite to delete the class instead
         self.list.clear()
+
+        self.listName = None
         self.list = None
 
     def insert(self, a):
---
+++ tests.py
@@ -0,0 +1,22 @@
+import ListAnalyzer as LAR
+
+# List Name
+# lar = LAR.ListAnalyzer("MyList")
+# print(lar.listName)
+
+# delete
+# lar = LAR.ListAnalyzer("MyList")
+# lar.delete()
+
+# insert
+# lar = LAR.ListAnalyzer("MyList")
+# lar.insert(123)
+
+# print(lar.list[0])
+
+# get_count
+# lar = LAR.ListAnalyzer("MyList")
+# lar.insert(69)
+
+# count = lar.get_count()
+# print(count)
